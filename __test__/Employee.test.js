2b0627aa6e6ddce18b5cd2163e96a03a1643bb75
Kevin Friday <kevinfriday87@gmail.com>
2022-06-11 12:12:36 -0500

    first test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -2,13 +2,12 @@ const Employee = require('../lib/Employee.js');
 
 
 test('creates a employee object and assigns',()=>{
-    const employee = new Employee('Tom');
+    const employee = new Employee('Tom',13,'tom@tom.com');
 
     expect(employee.name).toBe('Tom')
-    // expect(employee.id).toBe('13')
-    // expect(employee.email).toBe('tom@tom.com')
+    expect(employee.id).toBe('13')
+    expect(employee.email).toBe('tom@tom.com')
 
 })
 
 
-module.exports = Employee;
